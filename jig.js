f1860113aaaaea3d6588da3b4753cd7f2303af22
pivaldi <contact@piprime.fr>
2014-02-07 15:59:25 +0100

    Add jig.js file

---
+++ jig.js
@@ -0,0 +1,19 @@
+define([
+"./_Widget",
+  "./button/Action",
+  "./button/LazyDropDown",
+  "./util/async",
+  "./util/makeDOM",
+  "./input/_Container"
+], function() {
+
+  // module:
+  //		jig/jig
+
+  /*=====
+    return {
+    // summary:
+    //		A roll-up for common jig methods
+    };
+    =====*/
+});

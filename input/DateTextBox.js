ac666e8465941a6c360b380f21f0396618942a00
rvanlande <rvanlande@ovya.fr>
2016-06-08 09:29:56 +0200

    fix DateTextBox : update the calendar date

--- input/DateTextBox.js
+++ input/DateTextBox.js
@@ -3,47 +3,47 @@ define([
   "dojo/_base/declare",
   "dijit/form/DateTextBox",
   "dojo/date/stamp"
-], function(module, declare, DateTextBox, stamp) {
+], function (module, declare, DateTextBox, stamp) {
 
-/**
- * Extension of dijit/DataTextBox to deal with string date value
- *
- * It is used in the same way, except that it will work when set with
- * a string date value.
- *
- * The value getter is unchanged and returns a Date object.
- */
-return declare(DateTextBox,
-{
+  /**
+   * Extension of dijit/DataTextBox to deal with string date value
+   *
+   * It is used in the same way, except that it will work when set with
+   * a string date value.
+   *
+   * The value getter is unchanged and returns a Date object.
+   */
+  return declare(DateTextBox, {
 
-  timestamp: false,
-  autoSerialize: false,
-  serializeOptions: {
-    selector: "date"
-  },
+    timestamp: false,
+    autoSerialize: false,
+    serializeOptions: {
+      selector: "date"
+    },
 
-  _setValueAttr: function(value) {
-    if (typeof value == 'string') {
-      // var displayValue = value.replace(/Z/, '').replace(/-/g, '/');
-      var displayValue = value;
-      // var date = new Date(displayValue);
-      var date = stamp.fromISOString(displayValue);
-      this.set('value', date);
-    } else {
-      this.inherited(arguments);
-    }
-  },
+    _setValueAttr: function (value) {
 
-  _getValueAttr: function() {
-    var value = this.inherited(arguments);
-    if (value && this.autoSerialize) {
-      value = this.serialize(value, this.serializeOptions);
-    }
-    return value;
-  },
+      if (typeof value === 'string') {
+        var displayValue = value;
+        var date = stamp.fromISOString(displayValue);
+        this.set('value', date);
+        this.set('dropDownDefaultValue', date);
 
-  declaredClass: module.id
+      } else {
+        this.inherited(arguments);
+      }
+    },
 
-});
+    _getValueAttr: function () {
+      var value = this.inherited(arguments);
+      if (value && this.autoSerialize) {
+        value = this.serialize(value, this.serializeOptions);
+      }
+      return value;
+    },
+
+    declaredClass: module.id
+
+  });
 
 });
